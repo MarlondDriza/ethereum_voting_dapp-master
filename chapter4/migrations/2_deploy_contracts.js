76288deebd4405ca12bc9f1e3697f8abdcd3bf1a
JinFeng <57535660+MarlondDriza@users.noreply.github.com>
2022-03-27 09:19:39 +0800

    Update 2_deploy_contracts.js

--- chapter4/migrations/2_deploy_contracts.js
+++ chapter4/migrations/2_deploy_contracts.js
@@ -11,12 +11,4 @@ module.exports = function(deployer) {
   deployer.deploy(ECRecovery);
   deployer.link(ECRecovery, Voting);
   deployer.deploy(Voting, ['Alice', 'Bob', 'Carol'], [alice_
-adsfads
-asd
-asd
-adf
-adsf
-adsf
-asdf
-adsf
-ads
+
